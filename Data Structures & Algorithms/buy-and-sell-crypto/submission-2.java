a7f41d32a016e58d4ca4cc7938f2eb8b1ead6201
Lohith Kumar <55250895+Lohith120397@users.noreply.github.com>
2026-04-30 23:58:35 +0530

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.java
@@ -0,0 +1,54 @@
+/*
+121. Best Time to Buy and Sell Stock
+Easy
+Topics
+Company Tags
+Hints
+You are given an integer array prices where prices[i] is the price of NeetCoin on the ith day.
+
+You may choose a single day to buy one NeetCoin and choose a different day in the future to sell it.
+
+Return the maximum profit you can achieve. You may choose to not make any transactions, in which case the profit would be 0.
+
+Example 1:
+
+Input: prices = [10,1,5,6,7,1]
+
+Output: 6
+Explanation: Buy prices[1] and sell prices[4], profit = 7 - 1 = 6.
+
+Example 2:
+
+Input: prices = [10,8,7,5,2]
+
+Output: 0
+Explanation: No profitable transactions can be made, thus the max profit is 0.
+
+Constraints:
+
+1 <= prices.length <= 100
+0 <= prices[i] <= 100
+
+*/
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit = Integer.MIN_VALUE;
+        int i = 0;
+        int j = i + 1;
+
+        while (j < prices.length) {
+            if (prices[i] > prices[j]) {
+                i = j;
+                j++;
+            } else {
+                profit = Math.max(profit, prices[j] - prices[i]);
+                j++;
+            }
+        }
+
+        if(profit < 0)
+            return 0;
+
+        return profit;
+    }
+}
