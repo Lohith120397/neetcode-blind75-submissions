c11720dea238b7186289afdfc8cbd2e86fe472b5
Lohith Kumar <55250895+Lohith120397@users.noreply.github.com>
2026-05-04 23:46:14 +0530

    Add: minimum-window-with-characters - submission-0

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-0.java
@@ -0,0 +1,76 @@
+/*
+Minimum Window Substring
+Hard
+Topics
+Company Tags
+Hints
+Given two strings s and t, return the shortest substring of s such that every character in t, including duplicates, is present in the substring. If such a substring does not exist, return an empty string "".
+
+You may assume that the correct output is always unique.
+
+Example 1:
+
+Input: s = "OUZODYXAZV", t = "XYZ"
+
+Output: "YXAZ"
+Explanation: "YXAZ" is the shortest substring that includes "X", "Y", and "Z" from string t.
+
+Example 2:
+
+Input: s = "xyz", t = "xyz"
+
+Output: "xyz"
+Example 3:
+
+Input: s = "x", t = "xy"
+
+Output: ""
+Constraints:
+
+1 <= s.length <= 1000
+1 <= t.length <= 1000
+s and t consist of uppercase and lowercase English letters.
+*/
+class Solution {
+    public String minWindow(String s, String t) {
+        if (t.isEmpty()) return "";
+
+        Map<Character, Integer> countT = new HashMap<>();
+        Map<Character, Integer> window = new HashMap<>();
+        for (char c : t.toCharArray()) {
+            countT.put(c, countT.getOrDefault(c, 0) + 1);
+        }
+
+        int have = 0, need = countT.size();
+        int[] res = {-1, -1};
+        int resLen = Integer.MAX_VALUE;
+        int l = 0;
+
+        for (int r = 0; r < s.length(); r++) {
+            char c = s.charAt(r);
+            window.put(c, window.getOrDefault(c, 0) + 1);
+
+            if (countT.containsKey(c) && window.get(c).equals(countT.get(c))) {
+                have++;
+            }
+
+            while (have == need) {
+                if ((r - l + 1) < resLen) {
+                    resLen = r - l + 1;
+                    res[0] = l;
+                    res[1] = r;
+                }
+
+                char leftChar = s.charAt(l);
+                window.put(leftChar, window.get(leftChar) - 1);
+                if (countT.containsKey(leftChar) && window.get(leftChar) < countT.get(leftChar)) {
+                    have--;
+                }
+                l++;
+            }
+        }
+
+        return resLen == Integer.MAX_VALUE ? "" : s.substring(res[0], res[1] + 1);
+
+    }
+}
