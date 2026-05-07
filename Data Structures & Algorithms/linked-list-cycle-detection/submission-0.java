5d9fe97510c681cfda114fa4761c8b1f582a5baa
Lohith Kumar <55250895+Lohith120397@users.noreply.github.com>
2026-05-07 23:32:30 +0530

    Add: linked-list-cycle-detection - submission-0

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-0.java
@@ -0,0 +1,59 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+/*
+Linked List Cycle Detection
+Easy
+Topics
+Company Tags
+Hints
+Given the beginning of a linked list head, return true if there is a cycle in the linked list. Otherwise, return false.
+
+There is a cycle in a linked list if at least one node in the list can be visited again by following the next pointer.
+
+Internally, index determines the index of the beginning of the cycle, if it exists. The tail node of the list will set it's next pointer to the index-th node. If index = -1, then the tail node points to null and no cycle exists.
+
+Note: index is not given to you as a parameter.
+
+Example 1:
+
+
+
+Input: head = [1,2,3,4], index = 1
+
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+
+Example 2:
+
+
+
+Input: head = [1,2], index = -1
+
+Output: false
+Constraints:
+
+0 <= Length of the list <= 1000.
+-1000 <= Node.val <= 1000
+index is -1 or a valid index in the linked list.
+*/
+class Solution {
+    public boolean hasCycle(ListNode head) {
+         ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) return true;
+        }
+        return false;
+    }
+}
