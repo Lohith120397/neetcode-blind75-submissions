0ccf3aa82f991aa87e0423a56514563e70526dac
Lohith Kumar <55250895+Lohith120397@users.noreply.github.com>
2026-05-10 23:29:44 +0530

    Add: replace-elements-with-greatest-element-on-right-side - submission-0

---
+++ Data Structures & Algorithms/replace-elements-with-greatest-element-on-right-side/submission-0.java
@@ -0,0 +1,36 @@
+/*
+Replace Elements With Greatest Element On Right Side
+Easy
+Topics
+Company Tags
+You are given an array arr, replace every element in that array with the greatest element among the elements to its right, and replace the last element with -1.
+
+After doing so, return the array.
+
+Example 1:
+
+Input: arr = [2,4,5,3,1,2]
+
+Output: [5,5,3,2,2,-1]
+Example 2:
+
+Input: arr = [3,3]
+
+Output: [3,-1]
+Constraints:
+
+1 <= arr.length <= 10,000
+1 <= arr[i] <= 100,000
+*/
+class Solution {
+    public int[] replaceElements(int[] arr) {
+        int n = arr.length;
+        int[] ans = new int[n];
+        int rightMax = -1;
+        for (int i = n - 1; i >= 0; i--) {
+            ans[i] = rightMax;
+            rightMax = Math.max(rightMax, arr[i]);
+        }
+        return ans;
+    }
+}
