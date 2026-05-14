26f26cb37ded612310abfd69c43ddeb83b3b4a9f
Lohith Kumar <55250895+Lohith120397@users.noreply.github.com>
2026-05-14 23:33:46 +0530

    Add: longest-common-prefix - submission-0

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-0.java
@@ -0,0 +1,38 @@
+/*
+You are given an array of strings strs. Return the longest common prefix of all the strings.
+
+If there is no longest common prefix, return an empty string "".
+
+Example 1:
+
+Input: strs = ["bat","bag","bank","band"]
+
+Output: "ba"
+Example 2:
+
+Input: strs = ["dance","dag","danger","damage"]
+
+Output: "da"
+Example 3:
+
+Input: strs = ["neet","feet"]
+
+Output: ""
+Constraints:
+
+1 <= strs.length <= 200
+0 <= strs[i].length <= 200
+strs[i] is made up of lowercase English letters if it is non-empty.
+*/
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        for (int i = 0; i < strs[0].length(); i++) {
+            for (String s : strs) {
+                if (i == s.length() || s.charAt(i) != strs[0].charAt(i)) {
+                    return s.substring(0, i);
+                }
+            }
+        }
+        return strs[0];
+    }
+}
